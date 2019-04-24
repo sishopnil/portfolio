8063363b114a33cf2795f871c187460f202e3709
Shamiul Shopnil <shopnil16@gmail.com>
2019-04-24 19:49:02 +0600

    With two apps

---
+++ blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=225)
+    pub_date = models.DateField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/') 
